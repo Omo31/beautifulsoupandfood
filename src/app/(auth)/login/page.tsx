dc6e3167aa3df7e531ef78ba9e95d3ca76be8930
Omowumi Badmus <oluwagbengwumi@gmail.com>
2025-11-03 20:16:12 +0000

    remove the eye thing. i think it is still causing error because the sign

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -6,16 +6,9 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
-import { Eye, EyeOff } from 'lucide-react';
 
 export default function LoginPage() {
     const router = useRouter();
-    const [showPassword, setShowPassword] = useState(false);
-
-    const togglePasswordVisibility = () => {
-        setShowPassword(!showPassword);
-    }
 
     return (
         <>
@@ -35,13 +28,7 @@ export default function LoginPage() {
                             Forgot your password?
                         </Link>
                     </div>
-                    <div className="relative">
-                        <Input id="password" type={showPassword ? 'text' : 'password'} required />
-                        <Button type="button" variant="ghost" size="icon" className="absolute right-1 top-1 h-8 w-8" onClick={togglePasswordVisibility}>
-                            {showPassword ? <EyeOff className="h-4 w-4" /> : <Eye className="h-4 w-4" />}
-                            <span className="sr-only">{showPassword ? 'Hide password' : 'Show password'}</span>
-                        </Button>
-                    </div>
+                    <Input id="password" type="password" required />
                 </div>
                 <div className="grid grid-cols-2 gap-2">
                     <Button variant="outline" type="button" onClick={() => router.back()}>Cancel</Button>
