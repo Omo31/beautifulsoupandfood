501fdcad48988ff2782eff35bbaecf7e02174de6
Omowumi Badmus <oluwagbengwumi@gmail.com>
2025-11-07 14:14:06 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,10 +2,10 @@
 import type { FirebaseOptions } from 'firebase/app';
 
 export const firebaseConfig: FirebaseOptions = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "beautifulsoupandfood-198cd.firebaseapp.com",
-  projectId: "beautifulsoupandfood-198cd",
-  storageBucket: "beautifulsoupandfood-198cd.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  "projectId": "studio-5696680782-558b6",
+  "appId": "1:844060417221:web:19af7a31beb902afd424ac",
+  "apiKey": "AIzaSyCOeglxSgbHFN65OsMPif-nz0AM_ZKcvFo",
+  "authDomain": "studio-5696680782-558b6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "844060417221"
 };
